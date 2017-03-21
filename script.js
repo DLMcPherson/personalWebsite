de171ae0112fd99ce348232bcd2945e147e2877b
David McPherson <DavidMcP555@gmail.com>
2017-03-21 15:42:03 -0700

    rewrote and reorganized everything. Now with 400% more pictures!

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById("foot01").innerHTML =
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='index.html'>Research</a></li>" +
+"<li><a href='Me.html'>Me</a></li>" +
 "<li><a href='cv.html'>CV</a></li>"
 "</ul>";
