eadc695746aff6f527d74652d567409e4c09f0d4
David McPherson <DavidMcP555@gmail.com>
2017-03-21 13:37:49 -0700

    added colored sections

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " David L McPherson. All rights reserved.</p>";
 
 document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" + 
-"<li><a href='index.html'>Home</a></li>" + 
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
 "<li><a href='cv.html'>CV</a></li>"
 "</ul>";
